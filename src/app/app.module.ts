edb9d68a544004b70526a162c13dde14e7a5078e
joMahdi <joulali.mehdi@gmail.com>
2018-07-20 16:10:22 +0100

    creating front end sidebar and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProduitComponent } from './produit/produit.component';
 import { ProduitMockService } from './produit/produit.mock.servise';
+import { ProduitComponent } from './produit/produit.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule
